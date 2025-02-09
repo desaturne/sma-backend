07e3fbeca7075b3c6f7879e52d4a77adc7ddb049
Sushma Shrestha <153975721+desaturne@users.noreply.github.com>
2025-02-09 20:11:34 +0545

    create post

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,1 +1,32 @@
-import {Router} from "express";
+import { Router } from "express";
+import { createPosts, listPosts } from "../services/post.service.js";
+
+const router = Router()
+//localhost:3001/api/posts  => post request
+router.post("/", async (req, res) => {
+    try {
+        const result = await createPosts(req)
+        res.send(result)
+    } catch (error) {
+        console.log("Error at login: ", error)
+        res.status(400).send({
+            message: "Error Occured",
+            error: error
+        });
+    }
+})
+
+router.get("/", async (req, res) => {
+    try {
+        const result = await listPosts(req)
+        res.send(result)
+    } catch (error) {
+        console.log("Error at login: ", error)
+        res.status(400).send({
+            message: "Error Occured",
+            error: error
+        });
+    }
+})
+
+export default router;
--- services/post.service.js
+++ services/post.service.js
@@ -1,30 +1,29 @@
 import { PrismaClient } from '@prisma/client'
-import "dotenv/config.js"
 
 const prisma = new PrismaClient()
 
-const getPosts = async (req) =>{
-    const posts = await prisma.post.findMany()
-    return posts;
-}
-
-const getPost = async (req) =>{
-    const {postId} = req.params;
-    const post = await prisma.post.findFirst({
-        where:{
-            id: postId
+const createPosts = async (req) => {
+    const content = req.body.content
+    const result = await prisma.posts.create({
+        data: {
+            content,
+            userId: req.body.user.id
+        },
+        include: {
+            User: true
         }
     })
-    return post;
+    return { result };
 }
 
-const createPost = async (req) =>{
-    const {id, content} = req.body;
-    const post = await prisma.post.create({
-        data:{
-            id,
-            content
+const listPosts = async (req) => {
+    const result = await prisma.posts.findMany({
+        include: {
+            User: true
         }
     })
-    return post;
+    return result;
 }
+
+
+export { createPosts, listPosts }
